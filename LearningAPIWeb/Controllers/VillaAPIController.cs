97786d65fe711e55dfbf00fbf2d832b59665ddc1
MUHAMMADALI-USER <eshboevmuhammadali2@gmail.com>
2025-05-13 23:16:37 +0500

    Learned how to delete and how to update(only one version of updating)

--- LearningAPIWeb/Controllers/VillaAPIController.cs
+++ LearningAPIWeb/Controllers/VillaAPIController.cs
@@ -16,11 +16,10 @@ namespace LearningAPIWeb.Controllers
             return Ok(DachaStore.dachaList);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "Get Dacha")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-
         //[ProducesResponseType(200, Type = typeof(VillaDTO)]
         public ActionResult<DachaDTO> GetDachaById(int id)
         {
@@ -37,8 +36,6 @@ namespace LearningAPIWeb.Controllers
             return Ok(dacha);
         }
 
-
-
         [HttpGet("{name}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -54,8 +51,6 @@ namespace LearningAPIWeb.Controllers
             return Ok(dacha);
         }
 
-
-
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -63,6 +58,13 @@ namespace LearningAPIWeb.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<DachaDTO> CreateDacha([FromBody] DachaDTO dachaDTO)
         {
+            // Chek if object name is unique
+            if (DachaStore.dachaList.FirstOrDefault(u => u.Name.ToLower() == dachaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Dacha with this name already exists");
+                return BadRequest(ModelState);
+            }
+
             if (dachaDTO == null)
             {
                 return BadRequest(dachaDTO);
@@ -78,6 +80,47 @@ namespace LearningAPIWeb.Controllers
             return Ok(dachaDTO);
 
         }
-    
+
+
+        [HttpDelete("{id:int}", Name = "Delete Dacha")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<DachaDTO> DeleteDacha(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var dacha = DachaStore.dachaList.FirstOrDefault(u => u.Id == id);
+            if (dacha == null)
+            {
+                return NotFound();
+            }
+            DachaStore.dachaList.Remove(dacha);
+            return Ok(dacha);
+        }
+
+
+        [HttpPut("int:id", Name = "Update Dacha ")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<DachaDTO> UpdateDacha(int id, [FromBody] DachaDTO dachaDTO)
+        {
+            if (dachaDTO == null || id != dachaDTO.Id)
+            {
+                return BadRequest();
+            }
+            var dacha = DachaStore.dachaList.FirstOrDefault(u => u.Id == id);
+            if (dacha == null)
+            {
+                return NotFound();
+            }
+            dacha.Name = dachaDTO.Name;
+            dacha.Price = dachaDTO.Price;
+            dacha.Area = dachaDTO.Area;
+            return Ok();
+        }
     }
 }
--- LearningAPIWeb/Data/DachaStore.cs
+++ LearningAPIWeb/Data/DachaStore.cs
@@ -7,8 +7,8 @@ namespace LearningAPIWeb.Data
     {
         public static List<DachaDTO> dachaList = new List<DachaDTO>
         {
-            new DachaDTO { Id = 1, Name = "Katta Dacha"},
-            new DachaDTO { Id = 2, Name = "Kichkina dacha" }
+            new DachaDTO { Id = 1, Name = "Katta Dacha", Price = 10, Area = 100},
+            new DachaDTO { Id = 2, Name = "Kichkina dacha", Price = 20, Area = 200}
         };
     }
 }
